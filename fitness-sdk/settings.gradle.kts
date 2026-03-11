d1c280acac8bff4c4a3e985cb0dee549719c6a37
ItzRoXoR <146579961+ItzRoXoR@users.noreply.github.com>
2026-03-11 22:30:23 +0700

    fix setting.gradle
    fix 2

--- fitness-sdk/settings.gradle.kts
+++ fitness-sdk/settings.gradle.kts
@@ -1,7 +1,10 @@
 val localProps = file("local.properties")
 if (!localProps.exists()) {
-    val androidHome = System.getenv("ANDROID_HOME") ?: System.getenv("ANDROID_SDK_ROOT")
-    if (androidHome != null) {
+    val androidHome = System.getenv("ANDROID_HOME")
+        ?: System.getenv("ANDROID_SDK_ROOT")
+        ?: "${System.getProperty("user.home")}\\AppData\\Local\\Android\\Sdk"
+    val sdkDir = File(androidHome)
+    if (sdkDir.exists()) {
         localProps.writeText("sdk.dir=${androidHome.replace("\\", "\\\\")}\n")
     }
 }
